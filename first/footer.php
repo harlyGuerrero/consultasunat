9d8bb3fffeac0d507bfd9b858c11ab807d08fd86
harlyGuerrero <harlydavinsonguerrero@gmail.com>
2022-09-10 23:51:19 -0500

    ConsultaSunat

---
+++ first/footer.php
@@ -0,0 +1,15 @@
+        
+    </div>
+    <footer class="hdev__footer">
+            <div class="hdev__footer-container">
+                <div class="hdev__footer-content">
+                        <p>Copyright©2022 All rights reserved | hecho con <span><img src="img/heart.svg" alt=""></span> por Harly</p>
+                </div>
+            </div>
+    </footer>
+<script>
+    var eliminar = document.querySelector("div[style='text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;']");
+    eliminar.classList.add("anotherclass");
+</script>
+</body>
+</html>
---
+++ first/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RENIEC PERÚ</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <header class="hdev__nav">
+        <div class="hdev__nav-bar">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <ul class="nav-item-logo me-auto mb-2 mb-lg-0">
+                        <div class="nav-item-logo-img">
+                            <img  src="img/logo.png" alt="">
+                        </div>
+                    </ul>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Portal Sunat</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Consultas
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Consulta DNI</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Consulta RUC</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Consulta Tipo de Cambio</a></li>
+                        </ul>
+                        </li>
+                    </ul>
+                    
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
+                        <button class="btn btn-outline-danger" type="submit">Buscar</button>
+                    </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div class="page-container">
+        
